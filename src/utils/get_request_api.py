19c35eeba65184b44bc1c7580be44de2aa5258b9
Mateus Almeida <mateusalmeida881@gmail.com>
2025-07-30 14:17:59 -0300

    feat (methods): adicao dos metodos para construir url e o processo de extracao da api paginada

--- src/utils/get_request_api.py
+++ src/utils/get_request_api.py
@@ -1,6 +1,16 @@
+import os
+from urllib.parse import urlencode
+
 import requests
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
+
+
+def build_url(base_url, endpoint, params):
+    base_url += endpoint
+    query_string = urlencode(params)
+    base_url += query_string
+    return base_url
 
 
 def request(url, headers):
@@ -16,3 +26,20 @@ def request(url, headers):
     http.mount("http://", adapter)
     response = http.get(url=url, headers=headers)
     return response
+
+
+def offset_api(base_url, params, endpoint):
+    bearer_token = os.getenv("BEARER_TOKEN_PERFIT")
+    headers = {"Authorization": f"Bearer {bearer_token}"}
+    url = build_url(base_url=base_url, endpoint=endpoint, params=params)
+    raw_data = []
+    while url:
+        try:
+            response = request(url=url, headers=headers)
+            response = response.json()
+            raw_data.append(response)
+            url = response["paging"]["next"]
+        except Exception:
+            url = None
+
+    return raw_data
