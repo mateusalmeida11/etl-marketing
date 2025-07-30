366f5995898b76b09c4798c6d845e22dfe5bb329
Mateus Almeida <mateusalmeida881@gmail.com>
2025-07-30 11:26:40 -0300

    code (method): adicao do metodo info na funcao log

--- src/utils/logging_app.py
+++ src/utils/logging_app.py
@@ -1,3 +1,4 @@
+import datetime
 import logging
 
 
@@ -10,5 +11,15 @@ class LoggerAplication:
         self.storage_logs = []
         self.logger = logging.getLogger(name=name)
 
-    def _log(self, name):
-        pass
+    def _log(self, level, message):
+        self.logger.log(level=level, message=message)
+        dict_log = {
+            "timestamp": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            "name": self.logger.name,
+            "level": logging.getLevelName(level=level),
+            "message": message,
+        }
+        self.storage_logs.append(dict_log)
+
+    def info(self, message):
+        self._log(logging.INFO, message)
