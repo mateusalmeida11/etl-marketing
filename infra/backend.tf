b14253ed844ad11b5488b5fc451e35c0568186a5
Mateus Almeida <mateusalmeida881@gmail.com>
2025-08-20 15:46:50 -0300

    file: remocao de comentario

--- infra/backend.tf
+++ infra/backend.tf
@@ -4,6 +4,6 @@ terraform {
     key          = "dev/statefile.tfstate"
     region       = "us-east-1"
     encrypt      = true
-    use_lockfile = true  #S3 native locking
+    use_lockfile = true
   }
 }
