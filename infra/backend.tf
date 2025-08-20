665aef0e483903e3deb8b62fab189ae38c651697
Mateus Almeida <mateusalmeida881@gmail.com>
2025-08-20 09:50:31 -0300

    docs (backend terraform): criado arquivo para configurar Bucket S3 como backend

---
+++ infra/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket       = "mateusalmeida-us-east-1-terraform-statefile-with-lock"
+    key          = "dev/statefile.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true  #S3 native locking
+  }
+}
