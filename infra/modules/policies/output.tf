b52626af2fd9857df675b558d0a64895f8f9e80d
Mateus Almeida <mateusalmeida881@gmail.com>
2025-08-21 16:36:12 -0300

    file (new_output): saida ARN do scheduler

--- infra/modules/policies/output.tf
+++ infra/modules/policies/output.tf
@@ -2,3 +2,8 @@ output "lambda_execution_role_arn" {
     description = "ARN da role de execucao que sera anexada a Lambda"
     value = aws_iam_role.lambda_exec_role.arn
 }
+
+output "aws_scheduler_execution_role_arn" {
+    description = "Arn da role de execucao que sera utilizada no Scheduler"
+    value = aws_iam_role.event_bridge_role.arl
+}
